96785e1fa9b405f57d328680a0766137a8f7099e
Michelsaez610 <michel.saez@institutvidreres.cat>
2024-12-02 09:14:17 +0100

    comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { A2pageComponent } from "../components/a2page/a2page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, A2pageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { A2pageComponent } from '../components/a2page/a2page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: A2pageComponent}
+];
